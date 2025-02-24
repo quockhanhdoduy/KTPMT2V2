7bfbbc441685c54d0810a56e7ab59e438b7fc433
quockhanh.qkdd@gmail.com <quockhanh.qkdd@gmail.com>
2025-02-24 13:41:21 +0700

    task001 hotfix ...

---
+++ UniTest/src/main/java/com/mycompany/unitest/UniTest.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.unitest;
+
+/**
+ *
+ * @author admin
+ */
+public class UniTest {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
